13c74f532062c155cf8fc70b8263af43f2c543d6
ValenSaZu <111878177+ValenSaZu@users.noreply.github.com>
2023-08-26 12:51:17 -0500

    Add files via upload

---
+++ BusquedaPalabras/BusquedaPalabras/BusquedaPalabras.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int Busqueda(string (*p)[6], string a) {
+	for (int i{ 0 }; i < 5; i++) {
+		for (int j{ 0 }; j < 6; j++) {
+			
+		}
+	}
+	return 0;
+}
+
+int main()
+{
+	string A[5][6] = { {"A","J","P","A","T","O"},{"M", "L","O", "T", "A","P"},{"N", "T", "T", "H", "L", "O"},{"K","H", "A", "T", "O", "L"},{"T", "I", "P", "O", "J", "M"} };
+	Busqueda(A);
+	return 0;
+}
